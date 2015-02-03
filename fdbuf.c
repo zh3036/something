2e959eb5917dbe55c69890a70fa32b4c20bc2db9
zh3036 <zh3036@gmail.com>
2015-02-03 19:19:13 +0300

    try checkout

--- fdbuf.c
+++ fdbuf.c
@@ -5,8 +5,8 @@
 
 typedef struct buf {
   char buffer[MAXBUF];
-  char* bufptr;
-  int cnt;
+  char* bufptr;//
+  size_t cnt;//total num of content in buf
   struct buf *next;
   struct buf *prev;
 } fd_buf;
@@ -38,15 +38,34 @@ fd_buf* ini_buf(){
 void ini_fd(time_fd* tf, int fd){
   tf->fd=fd;
   tf->ini_time=tf->tms.tv_sec;
-  tf->fbuf = ini_buf();
+  tf->fbuf = NULL;
 }
 
-void add_buf(fd_buf* fb){
+void add_buf(time_fd* tf){
+
 ;}
 
-int bufread(time_fd* tf){
-  ;
+int bufread(time_fd* tf,size_t n){
+  if(!(tf->fbuf)) //create the first buf
+    tf->fbuf = ini_buf();
+  if(tf->fbuf->cnt==MAXBUF)
+    add_buf(tf);
+  //check how many space left
+  int empty=MAXBUF - (tf->fbuf->cnt % MAXBUF);
+  //min, decide how many can we read
+  int toread=empty;
+  if(toread>n) toread=n;
+  //see how many we actually read
+  int cnt=read(tf->fd, tf->fbuf->bufptr, toread);
+  switch(cnt){
+    case -1: return -1; // error
+    case 0: return 0;// EOF
+    default: 
+      tf->fbuf->cnt+=cnt;
+      tf->fbuf->bufptr+=cnt;
+  }
   return 0;
 }
+
 
 
